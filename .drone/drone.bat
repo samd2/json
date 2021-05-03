9d15ae8dd688aad12bbc159fc9226a4cbf4790c0
sdarwin <samuel.d.darwin@gmail.com>
2021-05-03 23:03:22 +0000

    test

--- .drone/drone.bat
+++ .drone/drone.bat
@@ -6,7 +6,9 @@ if "%DRONE_JOB_BUILDTYPE%" == "boost" (
 
 echo '==================================> INSTALL'
 
-git clone https://github.com/boostorg/boost-ci.git boost-ci-cloned --depth 1
+REM git clone https://github.com/boostorg/boost-ci.git boost-ci-cloned --depth 1
+REM testing
+git clone -b test8 https://github.com/sdarwin/boost-ci.git boost-ci-cloned --depth 1
 cp -prf boost-ci-cloned/ci .
 rm -rf boost-ci-cloned
 REM source ci/travis/install.sh
