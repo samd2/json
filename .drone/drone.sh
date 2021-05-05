8fc67d17cfe64db92803a5060adc74f8ff5b49d7
sdarwin <samuel.d.darwin@gmail.com>
2021-05-05 17:35:44 +0000

    testing

--- .drone/drone.sh
+++ .drone/drone.sh
@@ -5,102 +5,71 @@
 # (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
 
 set -e
-export TRAVIS_BUILD_DIR=$(pwd)
+
 export DRONE_BUILD_DIR=$(pwd)
-export TRAVIS_BRANCH=$DRONE_BRANCH
-export TRAVIS_EVENT_TYPE=$DRONE_BUILD_EVENT
 export VCS_COMMIT_ID=$DRONE_COMMIT
 export GIT_COMMIT=$DRONE_COMMIT
 export REPO_NAME=$DRONE_REPO
 export USER=$(whoami)
 export CC=${CC:-gcc}
 export PATH=~/.local/bin:/usr/local/bin:$PATH
+export TRAVIS_BUILD_DIR=$(pwd)
+export TRAVIS_BRANCH=$DRONE_BRANCH
+export TRAVIS_EVENT_TYPE=$DRONE_BUILD_EVENT
+
+common_install () {
+  git clone https://github.com/boostorg/boost-ci.git boost-ci-cloned --depth 1
+  cp -prf boost-ci-cloned/ci .
+  rm -rf boost-ci-cloned
+
+  if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+      unset -f cd
+  fi
+
+  export SELF=`basename $REPO_NAME`
+  export BOOST_CI_TARGET_BRANCH="$TRAVIS_BRANCH"
+  export BOOST_CI_SRC_FOLDER=$(pwd)
+
+  . ./ci/common_install.sh
+}
 
 if [ "$DRONE_JOB_BUILDTYPE" == "boost" ]; then
 
 echo '==================================> INSTALL'
 
-git clone https://github.com/boostorg/boost-ci.git boost-ci-cloned
-cp -prf boost-ci-cloned/ci .
-rm -rf boost-ci-cloned
-
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-    unset -f cd
-fi
-
-export SELF=`basename $REPO_NAME`
-export BOOST_CI_TARGET_BRANCH="$TRAVIS_BRANCH"
-export BOOST_CI_SRC_FOLDER=$(pwd)
-
-. ./ci/common_install.sh
+common_install
 
 echo '==================================> SCRIPT'
 
-$BOOST_ROOT/libs/$SELF/ci/travis/build.sh
+. $BOOST_ROOT/libs/$SELF/ci/build.sh
 
-elif [ "$DRONE_JOB_BUILDTYPE" == "fc4716b68c-0fcaf592f9" ]; then
+elif [ "$DRONE_JOB_BUILDTYPE" == "codecov" ]; then
 
 echo '==================================> INSTALL'
 
-git clone https://github.com/boostorg/boost-ci.git boost-ci-cloned
-cp -prf boost-ci-cloned/ci .
-rm -rf boost-ci-cloned
-
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-    unset -f cd
-fi
-
-export SELF=`basename $REPO_NAME`
-export BOOST_CI_TARGET_BRANCH="$TRAVIS_BRANCH"
-export BOOST_CI_SRC_FOLDER=$(pwd)
-
-. ./ci/common_install.sh
+common_install
 
 echo '==================================> SCRIPT'
 
 cd $BOOST_ROOT/libs/$SELF
 ci/travis/codecov.sh
 
-elif [ "$DRONE_JOB_BUILDTYPE" == "fc4716b68c-db180b7bd2" ]; then
+elif [ "$DRONE_JOB_BUILDTYPE" == "valgrind" ]; then
 
 echo '==================================> INSTALL'
 
-git clone https://github.com/boostorg/boost-ci.git boost-ci-cloned
-cp -prf boost-ci-cloned/ci .
-rm -rf boost-ci-cloned
-
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-    unset -f cd
-fi
-
-export SELF=`basename $REPO_NAME`
-export BOOST_CI_TARGET_BRANCH="$TRAVIS_BRANCH"
-export BOOST_CI_SRC_FOLDER=$(pwd)
-
-. ./ci/common_install.sh
+common_install
 
 echo '==================================> SCRIPT'
 
 cd $BOOST_ROOT/libs/$SELF
 ci/travis/valgrind.sh
 
-elif [ "$DRONE_JOB_BUILDTYPE" == "fc4716b68c-cce9827eb5" ]; then
+elif [ "$DRONE_JOB_BUILDTYPE" == "coverity" ]; then
 
 echo '==================================> INSTALL'
 
-git clone https://github.com/boostorg/boost-ci.git boost-ci-cloned
-cp -prf boost-ci-cloned/ci .
-rm -rf boost-ci-cloned
-
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-    unset -f cd
-fi
-
-export SELF=`basename $REPO_NAME`
-export BOOST_CI_TARGET_BRANCH="$TRAVIS_BRANCH"
-export BOOST_CI_SRC_FOLDER=$(pwd)
-
-. ./ci/common_install.sh
+common_install.sh
 
 echo '==================================> SCRIPT'
 
