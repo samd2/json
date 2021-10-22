e6d916fbcc64cac5b63fdeb2623e909c006f4d0b
sdarwin <samuel.d.darwin@gmail.com>
2021-10-22 10:16:06 -0500

    test codecov

--- .drone/drone.sh
+++ .drone/drone.sh
@@ -4,7 +4,7 @@
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
 
-set -e
+set -ex
 export TRAVIS_BUILD_DIR=$(pwd)
 export DRONE_BUILD_DIR=$(pwd)
 export TRAVIS_BRANCH=$DRONE_BRANCH
