3d088c6755e09776a1b150fcddbf60f380a7d820
sdarwin <samuel.d.darwin@gmail.com>
2022-03-09 14:39:15 -0600

    testing

--- .drone/drone.sh
+++ .drone/drone.sh
@@ -5,7 +5,9 @@
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
 
-set -e
+set -ex
+hostname
+
 export TRAVIS_OS_NAME=linux
 export TRAVIS_BUILD_DIR=$(pwd)
 export DRONE_BUILD_DIR=$(pwd)
